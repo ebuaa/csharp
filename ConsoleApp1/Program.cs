9e8d8ce70ba7156ec6a3e086bd0e5beef87ba5b5
ebuaa <axel.aube@viacesi.fr>
2024-03-25 09:51:00 +0100

    first commi

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Square(int a, int b)
+    {
+        a = a + 1;
+        b++;
+        a = a * a;
+        b += b;
+        Console.WriteLine(a + " " + b);
+    }
+
+    static void Main(string[] args)
+    {
+        int num1 = 5;
+        int num2 = 10;
+        Console.WriteLine(num1 + " " + num2);
+        Square(num1, num2);
+        Console.WriteLine(num1 + " " + num2);
+        Console.WriteLine("Press enter key to Exit..");
+        Console.ReadLine();
+    }
+}
